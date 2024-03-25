96dbd3ab8582e35e8c970e1caebed9d13d4e5d79
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-03-25 14:36:58 +0530

    some basics

--- App.js
+++ App.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, {useState} from 'react'
+import { View, Text, Image, StyleSheet, Button, TextInput, Alert } from 'react-native'
 
 const App = () => {
+  const [text, setText] = useState("")
+
+  const handleButton = () => {
+    Alert.alert('You types:', text)
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello worldhgmhgjh</Text>
-      <Image
-        source={{ uri: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600' }}
-        style={styles.image}
-      />
+      <Text>Hello woprld</Text>
+      <Image source={{ uri : 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600'}} style={styles.image} />
+      <TextInput  style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Type something..." />
+    <Button title="Press Me" onPress={handleButton} />
     </View>
-  );
-}
+  )
+}    
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'blue',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    
+    
   },
   image: {
     width: 200,
     height: 200,
-    borderRadius: 10,
-    marginTop: 20,
+    borderRadius: 20
   },
-});
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    margin: 10,
+    width: '80%',
+  }
+})
 
-export default App;
+export default App
